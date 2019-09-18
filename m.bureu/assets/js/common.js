71efe336c528bdfc51a4002809da70583e5326cf
Andrey <sindrome15@mail.ru>
2019-09-19 01:47:07 +0700

    19.09
    • Добавлена страница контактов
    • Поправлены ссылки в меню на всех страницах
    • Добавлено мобильное меню

--- m.bureu/assets/js/common.js
+++ m.bureu/assets/js/common.js
@@ -1,4 +1,47 @@
 $(document).ready(function () {
     // Photobox
     $('#gallery').photobox('a', { time : 0 });
+
+    // Валидация форм
+    $('.form-submit').on('click', function(e){
+        e.preventDefault();
+        var count_errors = 0;
+        var form = $(this).parent('form');
+
+        $('.required', form).each(function(i, el){
+            if($('input', el).val() == '' || $('textarea', el).val() == ''){
+                $(el).addClass('error');
+                count_errors++;
+            } else {
+                $(el).removeClass('error');
+            }
+        });
+
+        if(count_errors == 0){
+            form.submit();
+        }
+    });
+
+    // Мобильное меню
+    $('.header__hamburder-wrapper').on('click', function(){
+        $('body').toggleClass('mobile-open');
+
+        if($('.mobile-open').length > 0){
+            $('.mobile-menu').css({ 'display' : 'block' });
+            $('.mobile-menu').animate({
+                opacity: 1,
+            }, 300, function() {
+                // Animation complete.
+            });
+        } else {
+            $('.mobile-menu').animate({
+                opacity: 0,
+            }, 300, function() {
+                $('.mobile-menu').css({ 'display' : 'none' });
+            });
+            $('.mobile-menu ul li').each(function(i, el){
+                $(el).css({ 'opacity' : 0 });
+            });
+        }
+    });
 });
